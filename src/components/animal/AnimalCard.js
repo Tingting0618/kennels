98ea4910aa032f5587a87589d48fdd19586893e8
duan.tingting@yahoo.com <tingtingduan@tingtings-Air.attlocal.net>
2021-06-01 21:04:42 -0500

    chapter2_completed

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -7,3 +7,14 @@ export const AnimalCard = () => (
         <div className="animal__breed">Breed: Poodle</div>
     </section>
 )
+
+// import React from "react"
+// import "./Animal.css"
+
+
+// export const AnimalCard = ({ animal }) => (
+//     <section className="animal">
+//         <h3 className="animal__name">{animal.name}</h3>
+//         <address className="location__address">{animal.location.name}</address>
+//     </section>
+// )
